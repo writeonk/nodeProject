cec282457a58924ac59d68b1d05db78357bd6e19
kunal <kunalsoni9995@gmail.com>
2022-12-26 20:10:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function sayHello(name){
+    console.log('Hello ' +name);
+}
+
+sayHello('Kunal');
+
+console.log(); //global object
+
+setTimeout(); // delay
+clearTimeout();
+setInterval();
+clearInterval();
