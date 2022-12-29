14f012eeee47a0985793fc5a844c41381719ade8
maulikk04 <118893402+maulikk04@users.noreply.github.com>
2022-12-29 16:29:22 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+
+    console.log("Helllo");
+    document.write("Helllo World");
+    console.warn("warning");
+    console.error("error");
+    var n1 =5;
+    var n2=10;
+    console.log(n1 + n2);
+    var a;
+    console.log(a);
+    
