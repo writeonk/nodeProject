c9f79c7e02c9f6eca664a4ee232387c284f7bb58
maulikk04 <118893402+maulikk04@users.noreply.github.com>
2023-01-07 17:42:57 +0530

    guess the number game

---
+++ exe1.js
@@ -0,0 +1,24 @@
+// exe1 : guess the number
+
+let a = Math.floor((Math.random() * 100) + 1)
+let chances = 0
+
+for (; ;) {
+    let num = prompt("enter number:")
+
+    if (num == a) {
+        console.log("you have guessed right")
+        break
+    }
+    else if (num > a) {
+        chances++;
+        console.log("enter number is greater than actual number")
+        continue
+    }
+    else if (num < a) {
+        chances++;
+        console.log("enter number is smaller than actual number")
+        continue
+    }
+}
+console.log("your score is : " + (100 - chances) + " and the number is : " + a)
