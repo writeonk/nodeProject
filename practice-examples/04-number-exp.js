0653e510dc619b6f53435ee418c8e8588c066d4d
kunal <kunalsoni9995@gmail.com>
2023-01-15 18:40:04 +0530

    if ladder program

--- practice-examples/04-number-exp.js
+++ practice-examples/04-number-exp.js
@@ -15,12 +15,15 @@ let age = prompt("what's your age?")
 if (age <= 0) {
     alert("This is not valid age")
 }
-else if (age < 1 && age >=15) {
+else if (age >= 1 && age <= 15) {
     alert("Kid")
-} 
-else if (age > 15 && age < 25) {
+}
+else if (age >= 16 && age <= 25) {
     alert("Young")
-} 
-else {
+}
+else if (age >= 26 && age < 75) {
     alert("Adult")
+}
+else {
+    alert("Aged")
 }
